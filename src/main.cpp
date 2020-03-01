c6c971d0d690fe7aab2ea08f4dc33e9f8c908a95
gpaluk <garypaluk@gmail.com>
2020-03-01 18:36:27 +0000

    Housekeeping

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,6 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    //cout << "Hello, world" << endl;
-
-/**
-    ifstream inputFile ("src/_test.swf", ios::binary);
-    assert(inputFile);
-    inputFile.unsetf(ios::skipws);
-
-    istream_iterator<uint8_t> b(inputFile), e;
-    const vector<uint8_t> data(b, e);
-
-    //readFromRawData(data);
-
-    EX3::DataStream *stream = new EX3::DataStream(data);
-    uint8_t sig1 = stream->readUI8();
-    uint8_t sig2 = stream->readUI8();
-
-    cout << sig1 << endl;
-    cout << sig2 << endl;
-**/
     EX3::SwfParser p;
 	p.readFromFile("./data/test.swf");
 
