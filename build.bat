0e23834564bab5eb98f33f6f4681c8c88d55367c
gpaluk <garypaluk@gmail.com>
2020-03-01 18:40:54 +0000

    Use Emscripten zlib ports and remove thirdparty folder

--- build.bat
+++
@@ -1,4 +0,0 @@
-em++ -std=c++11 -Wc++11-extensions --preload-file src/_test.swf -o bin/vm.html -I/src/ -I/thirdparty/zlib/ src/DataStream.cpp src/Swf.cpp src/SwfParser.cpp src/main.cpp
--I${EM_HOME}/thirdparty/zlib -L${EM_HOME}/thirdparty/zlib
-
-REM python -m SimpleHTTPServer 8080
